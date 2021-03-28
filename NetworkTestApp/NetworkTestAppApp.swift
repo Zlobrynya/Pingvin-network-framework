70a8b5780c5269232c4c9f7a64cf7e01ca24a25b
Zlobrynya <zlobrynya@gmail.com>
2021-03-28 10:56:17 +0300

    First setup.

---
+++ NetworkTestApp/NetworkTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkTestAppApp.swift
+//  NetworkTestApp
+//
+//  Created by Nikita Nikitin on 28.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct NetworkTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
