3cc07ff65d38d38765b6645f42a6b15d22b21aae
Zlobrynya <zlobrynya@gmail.com>
2022-04-03 16:37:09 +0300

    Change access to public for property in PostRequest

--- Sources/NetworkFramework/Models/Requests/PostRequest.swift
+++ Sources/NetworkFramework/Models/Requests/PostRequest.swift
@@ -11,7 +11,7 @@ public class PostRequest: RequestProtocol {
  
     // MARK: - Private properties
     
-    let request: URLRequest
+    public let request: URLRequest
     private var task: Task<Data, Error>?
 
     // MARK: - External Dependencies
